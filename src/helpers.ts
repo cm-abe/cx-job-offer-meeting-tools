e2563455e9d4f35024294602acc92bb7dbc2f931
abe.shinsuke <abe.shinsuke@HL00302.local>
2020-04-03 09:47:31 +0900

    remove unused functino

--- src/helpers.ts
+++ src/helpers.ts
@@ -2,12 +2,6 @@ function hasProperty<K extends string>(x: unknown, name: K): x is { [M in K]: un
     return x instanceof Object && name in x;
 }
 
-function getMentionsUser(text: string): RegExpMatchArray {
-    const regIdPattern = /<@[A-Z0-9]{9}>/g;
-
-    return text.match(regIdPattern);
-}
-
 function checkJobOfferInformation(formData: FormStates): boolean {
     const regInvalidChannelNamePattern = /[ 　,\.、。]/g;
 
@@ -18,4 +12,4 @@ function generateJobOfferChannel(formData: FormStates):string {
     return `rec-cx-${formData.values.job_type.selected_job_type.selected_option.value}-${formData.values.applicant_name_kana.entered_applicant_name_kana.value}様`;
 }
 
-export {hasProperty, getMentionsUser, checkJobOfferInformation, generateJobOfferChannel};
+export {hasProperty, checkJobOfferInformation, generateJobOfferChannel};
